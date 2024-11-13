7304da2774f5316ebd7d336a7f855d1bf3720717
HotariTobu <hotari24tools@gmail.com>
2024-11-14 08:56:40 +0900

    add: set

---
+++ src/set.gd
@@ -0,0 +1,81 @@
+class_name Set
+
+var _data = {}
+
+
+func add(value: Variant) -> void:
+	_data[value] = null
+
+
+func clear() -> void:
+	_data.clear()
+
+
+func duplicate(deep: bool = false) -> Set:
+	var new_set = Set.new()
+	new_set._data = _data.duplicate(deep)
+	return new_set
+
+
+func erase(value: Variant) -> bool:
+	return _data.erase(value)
+
+
+func has(value: Variant) -> bool:
+	return _data.has(value)
+
+
+func has_all(values: Array) -> bool:
+	return _data.has_all(values)
+
+
+func hash() -> int:
+	return _data.hash()
+
+
+func is_empty() -> bool:
+	return _data.is_empty()
+
+
+func is_read_only() -> bool:
+	return _data.is_read_only()
+
+
+func make_read_only() -> void:
+	_data.make_read_only()
+
+
+func merge(another: Set, overwrite: bool = false) -> void:
+	_data.merge(another._data, overwrite)
+
+
+func merged(another: Set, overwrite: bool = false) -> Set:
+	var new_set = Set.new()
+	new_set._data = _data.merged(another._data, overwrite)
+	return new_set
+
+
+func recursive_equal(another: Set, recursion_count: int) -> bool:
+	var values = _data.keys()
+	var another_values = another._data.keys()
+	if values() != another_values:
+		return false
+
+	if recursion_count == 0:
+		return true
+
+	for index in range(len(values())):
+		var value = values[index]
+		if value is Dictionary or value is Set:
+			if not value.recursive_equal(another_values[index], recursion_count - 1):
+				return false
+
+	return true
+
+
+func size() -> int:
+	return _data.size()
+
+
+func values() -> Array:
+	return _data.keys()
