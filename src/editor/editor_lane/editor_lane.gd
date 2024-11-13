4880bf7a7a91e83590bf3c059bf448a106830c3c
HotariTobu <hotari24tools@gmail.com>
2024-11-14 08:58:47 +0900

    upd: EditorLane

--- src/editor/editor_lane/editor_lane.gd
+++ src/editor/editor_lane/editor_lane.gd
@@ -1,66 +1,88 @@
 class_name EditorLane
-extends Lane
+extends EditorSelectable
 
-signal notified(property: StringName)
+var _editor_global = editor_global
 
-var selecting: bool = false:
+var _vertices: Array[VertexData]:
 	get:
-		return selecting
-	set(value):
-		selecting = value
-		_update_selecting()
+		return _vertices
+	set(next):
+		var prev = _vertices
+		_vertices = next
+		_remove_segment_collision_shapes(prev)
+		_add_segment_collision_shapes(next)
+		queue_redraw()
 
-func _init(use_collision: bool = true):
-	speed_limit = 60
-	next_option_dict = {}
+var _collision_shape_dict = {}
 
-	if use_collision:
-		_setup_collision_shapes.call_deferred()
 
-func from_dict(dict: Dictionary):
-	super(dict)
+func _init(data: LaneData):
+	var source = _editor_global.source_db.get_or_add(data)
+	source.bind("vertex_ids").using(_get_vertices).to(self, "_vertices")
 
-	for lane in next_option_dict:
-		var option = next_option_dict[lane]
-		next_option_dict[lane] = Option.new(option)
 
-func add_next_lane(lane: EditorLane):
-	var option = Lane.Option.new()
-	option.weight = 1
-	next_option_dict[lane] = Option.new(option)
-	notified.emit(&'next_option_dict')
+func _draw():
+	var color: Color
+	if selecting:
+		color = setting.lane_selecting_color
+	elif selected:
+		color = setting.lane_selected_color
+	else:
+		return
 
-func remove_next_lane(lane: EditorLane):
-	next_option_dict.erase(lane)
-	notified.emit(&'next_option_dict')
+	var points = _vertices.map(VertexData.to_vector)
+	if len(points) < 2:
+		return
 
-func _update_selecting():
-	for child in get_children():
-		var item = child as EditorSelectable
-		item.selecting = selecting
+	var width = setting.selection_radius / zoom_factor
+	var radius = width / 2
+	draw_circle(points[0], radius, color)
+	draw_polyline(points, color, width)
+	draw_circle(points[-1], radius, color)
 
-func _setup_collision_shapes():
-	var segments = EditorLaneSegments.new(self)
-	add_child(segments)
 
-	var last_point = EditorLanePoint.new(self, 0)
-	add_child(last_point)
+func _get_vertices(vertex_ids: Array[StringName]) -> Array[VertexData]:
+	return vertex_ids.map(_editor_global.vertex_dict.get)
 
-	for index in range(1, curve.point_count):
-		var point = EditorLanePoint.new(self, index)
-		add_child(point)
 
-		segments.add_segment(last_point.position, point.position)
-		last_point = point
+func _add_segment_collision_shapes(vertices: Array[VertexData]):
+	var sources = vertices.map(_editor_global.source_db.get_or_add)
 
-class Option:
-	extends BindingSource
+	for index in range(len(sources) - 1):
+		var source0 = sources[index]
+		var source1 = sources[index + 1]
 
-	func _init(option: Lane.Option):
-		super(option)
+		var segment_shape = SegmentShape2D.new()
+		source0.bind("pos").to(segment_shape, "a")
+		source1.bind("pos").to(segment_shape, "b")
 
-	func to_dict() -> Dictionary:
-		return _source_object.to_dict()
+		var collision_shape = CollisionShape2D.new()
+		collision_shape.shape = segment_shape
 
-	func from_dict(dict: Dictionary):
-		_source_object.from_dict(dict)
+		add_child(collision_shape)
+		_collision_shape_dict[source0] = collision_shape
+
+	for source in sources:
+		source.add_callback("x", queue_redraw)
+		source.add_callback("y", queue_redraw)
+
+
+func _remove_segment_collision_shapes(vertices: Array[VertexData]):
+	var sources = vertices.map(_editor_global.source_db.get_or_add)
+
+	for index in range(len(sources) - 1):
+		var source0 = sources[index]
+		var source1 = sources[index + 1]
+
+		var collision_shape = _collision_shape_dict[source0]
+		var segment_shape = collision_shape.shape
+
+		source0.unbind("pos").from(segment_shape, "a")
+		source1.unbind("pos").from(segment_shape, "b")
+
+		remove_child(collision_shape)
+		_collision_shape_dict.erase(source0)
+
+	for source in sources:
+		source.remove_callback("x", queue_redraw)
+		source.remove_callback("y", queue_redraw)
