fc8e7cdaa96c2061f1233507aa4dd65bc54edba7
HotariTobu <hotari24tools@gmail.com>
2024-11-15 07:51:52 +0900

    fix: lane property panel

--- src/editor/editor_lane/editor_property_panel_content_lane.gd
+++ src/editor/editor_lane/editor_property_panel_content_lane.gd
@@ -2,11 +2,11 @@ extends EditorPropertyPanelContent
 
 const WEIGHT_LABEL = "Weight %s"
 
-var lanes: Array[BindingSource]:
+var lane_sources: Array[EditorBindingSource]:
 	get:
-		return lanes
+		return lane_sources
 	set(next):
-		var prev = lanes
+		var prev = lane_sources
 
 		if not prev.is_empty():
 			_unbind_cells(prev)
@@ -14,23 +14,25 @@ var lanes: Array[BindingSource]:
 		if not next.is_empty():
 			_bind_cells(next)
 
-		lanes = next
+		lane_sources = next
 
 var _editor_global = editor_global
 
-var _option_cells: Array[Control] = []:
+var _option_cells: Array[Control]:
 	get:
 		return _option_cells
-	set(new_option_cells):
-		for cell in _option_cells:
+	set(next):
+		var prev = _option_cells
+
+		for cell in prev:
 			cell.queue_free()
 
-		for cell in new_option_cells:
+		for cell in next:
 			add_child(cell)
 
-		_option_cells = new_option_cells
+		_option_cells = next
 
-@onready var _lane_selector = LaneSelector.new(_editor_global.source_db)
+@onready var _root = get_tree().root
 @onready var _speed_limit_box = $SpeedLimitBox
 
 
@@ -40,112 +42,100 @@ func get_target_type():
 
 func activate():
 	super()
-	_editor_global.data.bind('selected_items').using(_lane_selector).to(self, 'lanes')
+	_editor_global.data.bind(&"selected_items").using(_get_lanes).to(self, &"lane_sources")
+
 
 func deactivate():
 	super()
-	_editor_global.data.unbind('selected_items').from(self, 'lanes')
-	lanes = []
+	_editor_global.data.unbind(&"selected_items").from(self, &"lane_sources")
+	lane_sources = []
 
-func _bind_cells(_lanes: Array[BindingSource]):
-	var first_lane = _lanes.front() as BindingSource
 
-	var unity_converter = UnifyConverter.new(first_lane, 'speed_limit')
+func _get_lanes(items: Array[EditorSelectable]) -> Array[EditorBindingSource]:
+	var sources: Array[EditorBindingSource] = []
 
-	for lane in _lanes:
-		lane.bind('speed_limit').using(unity_converter).to(_speed_limit_box, 'value')
+	for item in items:
+		var segments = item as EditorLaneSegments
+		if segments == null:
+			continue
 
-	if len(_lanes) == 1:
-		first_lane.bind('next_option_dict').using(_create_option_cells).to(self, '_option_cells')
+		var lane = segments.lane
+		var source = _editor_global.source_db.get_or_add(lane, &"notified")
+		sources.append(source)
 
-func _unbind_cells(_lanes: Array[BindingSource]):
-	var first_lane = _lanes.front() as BindingSource
+	return sources
 
-	for lane in _lanes:
-		lane.unbind('speed_limit').from(_speed_limit_box, 'value')
 
-	if len(_lanes) == 1:
-		first_lane.unbind('next_option_dict').from(self, '_option_cells')
+func _bind_cells(sources: Array[EditorBindingSource]):
+	var first_source = sources.front() as EditorBindingSource
+
+	var unity_converter = UnifyConverter.new(first_source, &"speed_limit")
+
+	for sourcce in sources:
+		sourcce.bind(&"speed_limit").using(unity_converter).to(_speed_limit_box, &"value")
+
+	if len(sources) == 1:
+		first_source.bind(&"next_option_dict").using(_create_option_cells).to(self, &"_option_cells")
+
+
+func _unbind_cells(sources: Array[EditorBindingSource]):
+	var first_source = sources.front() as EditorBindingSource
+
+	for sourcce in sources:
+		sourcce.unbind(&"speed_limit").from(_speed_limit_box, &"value")
+
+	if len(sources) == 1:
+		first_source.unbind(&"next_option_dict").from(self, &"_option_cells")
 		_option_cells = []
+
 
 func _create_option_cells(option_dict: Dictionary):
 	var option_cells: Array[Control] = []
 
-	for lane in option_dict:
-		var option = option_dict[lane]
-		option_cells += _create_option_cell(lane, option)
+	for lane_id in option_dict:
+		var option = option_dict[lane_id]
+		var option_source = _editor_global.source_db.get_or_add(option)
+		option_cells += _create_option_cell(lane_id, option_source)
 
 	return option_cells
 
-func _create_option_cell(lane: EditorLane, option: BindingSource):
-	var index = lane.get_index()
 
+func _create_option_cell(lane_id: StringName, option_source: EditorBindingSource) -> Array[Control]:
 	var weight_label = Label.new()
-	weight_label.text = WEIGHT_LABEL % (index + 1)
+	weight_label.text = WEIGHT_LABEL % lane_id.right(2)
 	weight_label.mouse_filter = Control.MOUSE_FILTER_PASS
-	weight_label.mouse_entered.connect(_on_weight_row_mouse_entered.bind(lane))
-	weight_label.mouse_exited.connect(_on_weight_row_mouse_exited.bind(lane))
+	_connect_hover_signals_for_selecting(weight_label, lane_id)
 
 	var weight_box = WeightBox.new()
-	option.bind('weight').to(weight_box, 'value')
-	weight_box.mouse_entered.connect(_on_weight_row_mouse_entered.bind(lane))
-	weight_box.mouse_exited.connect(_on_weight_row_mouse_exited.bind(lane))
-	weight_box.value_changed.connect(_on_weight_box_value_changed.bind(lane))
+	option_source.bind(&"weight").to(weight_box, &"value")
+	_connect_hover_signals_for_selecting(weight_box, lane_id)
+	weight_box.value_changed.connect(_on_weight_box_value_changed.bind(option_source))
 
 	return [weight_label, weight_box] as Array[Control]
+
 
 func _on_speed_limit_box_value_changed(new_value: float):
 	_editor_global.undo_redo.create_action("Change lane speed limit")
 
-	for lane in lanes:
-		_editor_global.undo_redo.add_do_property(lane, 'speed_limit', new_value)
-		_editor_global.undo_redo.add_undo_property(lane, 'speed_limit', lane.speed_limit)
+	for source in lane_sources:
+		_editor_global.undo_redo.add_do_property(source, &"speed_limit", new_value)
+		_editor_global.undo_redo.add_undo_property(source, &"speed_limit", source.speed_limit)
 
 	_editor_global.undo_redo.commit_action()
 
-func _on_weight_box_value_changed(new_value: float, option_lane: EditorLane):
-	var lane = lanes.front()
-	var set_weight = func(value: float):
-		var option = lane.next_option_dict[option_lane]
-		option.weight = value
 
-	var current_option = lane.next_option_dict[option_lane]
-	var current_value = current_option.weight
-
+func _on_weight_box_value_changed(new_value: float, option_source: EditorBindingSource):
 	_editor_global.undo_redo.create_action("Change lane option weight")
-	_editor_global.undo_redo.add_do_method(set_weight.bind(new_value))
-	_editor_global.undo_redo.add_undo_method(set_weight.bind(current_value))
+	_editor_global.undo_redo.add_do_property(option_source, &"weight", new_value)
+	_editor_global.undo_redo.add_undo_property(option_source, &"weight", option_source.weight)
 	_editor_global.undo_redo.commit_action()
 
-static func _on_weight_row_mouse_entered(lane: EditorLane):
-	lane.selecting = true
 
+func _connect_hover_signals_for_selecting(control: Control, selectable_id: StringName):
+	var selectable = _root.get_node("%" + selectable_id) as EditorSelectable
+	control.mouse_entered.connect(func(): selectable.selecting = true)
+	control.mouse_exited.connect(func(): selectable.selecting = false)
 
-static func _on_weight_row_mouse_exited(lane: EditorLane):
-	lane.selecting = false
-
-class LaneSelector:
-	extends BindingConverter
-
-	var _source_db: BindingSourceDB
-
-	func _init(source_db: BindingSourceDB):
-		_source_db = source_db
-
-	func source_to_target(source_value: Variant) -> Variant:
-		var items = source_value as Array[EditorSelectable]
-		var lanes: Array[BindingSource] = []
-
-		for item in items:
-			var segments = item as EditorLaneSegments
-			if segments == null:
-				continue
-
-			var lane = segments.lane
-			var source = _source_db.get_or_add(lane)
-			lanes.append(source)
-
-		return lanes
 
 class WeightBox:
 	extends NumericBox
