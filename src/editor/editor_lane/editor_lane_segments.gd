c6d4a9bbdcc86ebdf2060ef0d1d4ec0ee6019ed9
HotariTobu <hotari24tools@gmail.com>
2024-11-14 20:54:19 +0900

    upd: lane nodes

--- src/editor/editor_lane/editor_lane_segments.gd
+++ src/editor/editor_lane/editor_lane_segments.gd
@@ -1,19 +1,46 @@
 class_name EditorLaneSegments
 extends EditorSelectable
 
-var lane: EditorLane
+var lane: EditorLaneData:
+	get:
+		return _lane
+
+var _editor_global = editor_global
+
+var _lane: EditorLaneData
+
+var _collision_shape_dict: Dictionary
 
 var _center: Vector2
 
-func _init(_lane: EditorLane):
+
+var _vertices: Array[VertexData]:
+	get:
+		return _vertices
+	set(next):
+		var prev = _vertices
+		_vertices = next
+		_remove_segment_collision_shapes(prev)
+		_add_segment_collision_shapes(next)
+		queue_redraw()
+		_update_center()
+
+func _init(data: EditorLaneData):
 	super(EditorPhysicsLayer.LANE_SEGMENTS)
+	
+	_lane = data
 
-	lane = _lane
+	var source = _editor_global.source_db.get_or_add(data)
+	source.bind("vertex_ids").using(_get_vertices).to(self, "_vertices")
 
-	var length = lane.curve.get_baked_length()
-	_center = lane.curve.sample_baked(length / 2)
 
 func _draw():
+	var points = _vertices.map(VertexData.pos_of)
+	if len(points) < 2:
+		return
+
+	draw_polyline(points, setting.lane_color, setting.lane_width)
+
 	var color: Color
 	if selecting:
 		color = setting.lane_selecting_color
@@ -22,24 +49,71 @@ func _draw():
 	else:
 		return
 
-	var points = lane.curve.get_baked_points()
 	var width = setting.selection_radius / zoom_factor
 	var radius = width / 2
 	draw_circle(points[0], radius, color)
 	draw_polyline(points, color, width)
 	draw_circle(points[-1], radius, color)
 
-func _to_string():
-	return "EditorLaneSegments(Lane: %s)" % lane
 
 func get_center() -> Vector2:
 	return _center
 
-func add_segment(pos1: Vector2, pos2: Vector2):
-	var segment_shape = SegmentShape2D.new()
-	segment_shape.a = pos1
-	segment_shape.b = pos2
+func _get_vertices(vertex_ids: Array[StringName]) -> Array[VertexData]:
+	var vertices: Array[VertexData]
+	for vertex_id in vertex_ids:
+		var vertex = _editor_global.vertex_db.get_of(vertex_id)
+		vertices.append(vertex)
+	return vertices
 
-	var collision_shape = CollisionShape2D.new()
-	collision_shape.shape = segment_shape
-	add_child(collision_shape)
+
+func _add_segment_collision_shapes(vertices: Array[VertexData]):
+	var sources = vertices.map(_editor_global.source_db.get_or_add)
+
+	for index in range(len(sources) - 1):
+		var source0 = sources[index]
+		var source1 = sources[index + 1]
+
+		var segment_shape = SegmentShape2D.new()
+		source0.bind("pos").to(segment_shape, "a")
+		source1.bind("pos").to(segment_shape, "b")
+
+		var collision_shape = CollisionShape2D.new()
+		collision_shape.shape = segment_shape
+
+		add_child(collision_shape)
+		_collision_shape_dict[source0] = collision_shape
+
+	for source in sources:
+		source.add_callback("x", queue_redraw)
+		source.add_callback("y", queue_redraw)
+
+
+func _remove_segment_collision_shapes(vertices: Array[VertexData]):
+	var sources = vertices.map(_editor_global.source_db.get_or_add)
+
+	for index in range(len(sources) - 1):
+		var source0 = sources[index]
+		var source1 = sources[index + 1]
+
+		var collision_shape = _collision_shape_dict[source0]
+		var segment_shape = collision_shape.shape
+
+		source0.unbind("pos").from(segment_shape, "a")
+		source1.unbind("pos").from(segment_shape, "b")
+
+		remove_child(collision_shape)
+		_collision_shape_dict.erase(source0)
+
+	for source in sources:
+		source.remove_callback("x", queue_redraw)
+		source.remove_callback("y", queue_redraw)
+
+
+func _update_center():
+	var curve = Curve2D.new()
+	for vertex in _vertices:
+		curve.add_point(vertex.pos)
+	
+	var length = curve.get_baked_length()
+	_center = curve.sample_baked(length / 2)
---
+++ src/editor/editor_lane/editor_lane_vertex.gd
@@ -0,0 +1,52 @@
+class_name EditorLaneVertex
+extends EditorSelectable
+
+enum Type { START, WAY, END }
+
+var type = Type.WAY
+
+var vertex: VertexData:
+	get:
+		return _vertex
+
+var _vertex: VertexData
+
+
+func _init(data: VertexData):
+	super(EditorPhysicsLayer.LANE_VERTEX)
+
+	_vertex = data
+
+	position = data.pos
+	
+	var circle_shape = CircleShape2D.new()
+	circle_shape.radius = 0
+
+	var collision_shape = CollisionShape2D.new()
+	collision_shape.shape = circle_shape
+	add_child(collision_shape)
+
+
+func _draw():
+	var color: Color
+	if selecting:
+		match type:
+			Type.START:
+				color = setting.lane_start_point_selecting_color
+			Type.WAY:
+				color = setting.lane_way_point_selecting_color
+			Type.END:
+				color = setting.lane_end_point_selecting_color
+	elif selected:
+		match type:
+			Type.START:
+				color = setting.lane_start_point_selected_color
+			Type.WAY:
+				color = setting.lane_way_point_selected_color
+			Type.END:
+				color = setting.lane_end_point_selected_color
+	else:
+		return
+
+	var radius = setting.selection_radius / zoom_factor
+	draw_circle(Vector2.ZERO, radius, color)
