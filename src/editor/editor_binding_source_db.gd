70d516ba7523454973a84e50ed685792b727d48c
HotariTobu <hotari24tools@gmail.com>
2024-11-14 21:18:42 +0900

    add: assertion not to forget specifying notify_signal

--- src/editor/editor_binding_source_db.gd
+++ src/editor/editor_binding_source_db.gd
@@ -3,4 +3,5 @@ class_name EditorBindingSourceDB
 var _source_dict = {}
 
 func get_or_add(object, notify_signal = null) -> EditorBindingSource:
+	assert(object is not Object or not object.has_signal('notified') or notify_signal == 'notified')
 	return _source_dict.get_or_add(object, EditorBindingSource.new(object, notify_signal))
