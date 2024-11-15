6a65e5b106794202d1000eec772f9985693d3c8f
HotariTobu <hotari24tools@gmail.com>
2024-11-15 17:10:01 +0900

    add: EditorContent

---
+++ src/editor/editor_content.gd
@@ -0,0 +1,29 @@
+class_name EditorContent
+extends EditorSelectable
+
+var data: ContentData:
+	get:
+		return _data
+
+var _editor_global = editor_global
+
+var _data: ContentData
+var _source: EditorBindingSource
+
+
+func _init(content: ContentData, layer: int):
+	super(layer)
+	unique_name_in_owner = true
+	name = content.id
+
+	_data = content
+	_source = _editor_global.source_db.get_or_add(content)
+
+
+func _ready():
+	super()
+	owner = get_tree().root
+
+
+static func data_of(node: EditorContent) -> ContentData:
+	return node._data
