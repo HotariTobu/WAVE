e192e6f167628471832afd20c0f2da5e1e8e31bb
HotariTobu <hotari24tools@gmail.com>
2024-11-16 16:04:59 +0900

    fix: spell

--- src/editor/editor_ui/editor_menu_action/editor_menu_action_read_write.gd
+++ src/editor/editor_ui/editor_menu_action/editor_menu_action_read_write.gd
@@ -10,9 +10,9 @@ func save_network():
 	if path == '':
 		$NetworkSaveFileDialog.show()
 		return
-		
+
 	_write_network(path)
-	
+
 func save_network_as():
 	$NetworkSaveFileDialog.show()
 
@@ -23,44 +23,43 @@ func _on_network_open_file_dialog_file_selected(path):
 
 func _on_network_save_file_dialog_file_selected(path):
 	_write_network(path)
-	
-	
+
+
 func _read_network(path: String):
 	var file = FileAccess.open(path, FileAccess.READ)
 	if file == null:
 		var error = FileAccess.get_open_error()
-		_show_error('Faild to open file', error)
+		_show_error('Failed to open file', error)
 		return
-		
+
 	var network = file.get_var()
 	_editor_global.from_dict(network)
-	
+
 	file.close()
-	
+
 	_editor_global.network_file_path = path
-	
-	
+
+
 func _write_network(path: String):
 	var file = FileAccess.open(path, FileAccess.WRITE)
 	if file == null:
 		var error = FileAccess.get_open_error()
-		_show_error('Faild to open file', error)
+		_show_error('Failed to open file', error)
 		return
-		
+
 	var network = _editor_global.to_dict()
 	file.store_var(network)
-	
+
 	file.close()
-	
+
 	_editor_global.network_file_path = path
-	
-	
+
+
 func _show_error(message: String, error = OK):
 	var text = message if error == OK else '%s: %s' % [
 		message,
 		error_string(error),
 	]
-	
+
 	$ErrorAcceptDialog.dialog_text = text
 	$ErrorAcceptDialog.show()
-	
