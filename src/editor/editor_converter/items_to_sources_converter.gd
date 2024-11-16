f0918a06693f155595c0d7b2d50f385c4353df04
HotariTobu <hotari24tools@gmail.com>
2024-11-16 16:42:49 +0900

    fix: to check scripts recursively

--- src/editor/editor_converter/items_to_sources_converter.gd
+++ src/editor/editor_converter/items_to_sources_converter.gd
@@ -15,10 +15,21 @@ func source_to_target(source_value: Variant) -> Variant:
 	var sources: Array[EditorBindingSource] = []
 
 	for item in items:
-		if not _content_node_script.instance_has(item):
+		if not isinstance(item, _content_node_script):
 			continue
 
 		var source = _editor_global.source_db.get_or_add(item.data)
 		sources.append(source)
 
 	return sources
+
+
+static func isinstance(instance: Object, script: Script) -> bool:
+	var instance_script = instance.get_script()
+	while instance_script != null:
+		if instance_script == script:
+			return true
+
+		instance_script = instance_script.get_base_script()
+
+	return false
