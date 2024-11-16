1d0577ca9af42c740659720a3b128ba016f59d2d
HotariTobu <hotari24tools@gmail.com>
2024-11-16 11:07:09 +0900

    upd: to use base select tool

--- src/editor/editor_tool/editor_tool_select.gd
+++ src/editor/editor_tool/editor_tool_select.gd
@@ -1,43 +1,7 @@
-extends EditorTool
+extends "res://src/editor/editor_tool/editor_tool_base_select.gd"
 
 const TOOL_DISPLAY_NAME = "Select tool"
 const TOOL_STATUS_HINT = "Left click: select, Shift + Left click: add/remove select"
-
-var _editor_global = editor_global
-
-var _hovered_items: Array[EditorSelectable]
-var _last_hovered_item: EditorSelectable:
-	get:
-		return _last_hovered_item
-	set(next):
-		var prev = _last_hovered_item
-
-		if prev != null:
-			prev.selecting = false
-
-		if next != null:
-			next.selecting = true
-
-		_last_hovered_item = next
-
-
-func _ready():
-	set_process_unhandled_input(false)
-
-
-func _unhandled_input(event: InputEvent):
-	if event is InputEventMouseMotion:
-		_pointer_area.position = get_local_mouse_position()
-
-	elif event is InputEventMouseButton:
-		if event.button_index == MOUSE_BUTTON_LEFT and not event.pressed:
-			if event.shift_pressed:
-				_toggle_selection()
-			else:
-				_replace_selection()
-
-	elif event.is_action_pressed(&"ui_cancel"):
-		_cancel()
 
 
 func get_display_name() -> String:
@@ -46,59 +10,3 @@ func get_display_name() -> String:
 
 func get_status_hint() -> String:
 	return TOOL_STATUS_HINT
-
-
-func activate() -> void:
-	set_process_unhandled_input(true)
-
-	_pointer_area.collision_mask = EditorPhysicsLayer.SELECTABLE
-
-	_pointer_area.area_entered.connect(_on_pointer_area_area_entered)
-	_pointer_area.area_exited.connect(_on_pointer_area_area_exited)
-
-
-func deactivate() -> void:
-	set_process_unhandled_input(false)
-
-	_pointer_area.collision_mask = 0
-
-	_pointer_area.area_entered.disconnect(_on_pointer_area_area_entered)
-	_pointer_area.area_exited.disconnect(_on_pointer_area_area_exited)
-
-	_hovered_items.clear()
-	_last_hovered_item = null
-
-
-func _on_pointer_area_area_entered(area):
-	var item = area as EditorSelectable
-	_hovered_items.append(item)
-	_last_hovered_item = item
-
-
-func _on_pointer_area_area_exited(area):
-	var item = area as EditorSelectable
-	_hovered_items.erase(item)
-	if _hovered_items.is_empty():
-		_last_hovered_item = null
-	else:
-		_last_hovered_item = _hovered_items.back()
-
-
-func _replace_selection():
-	_cancel()
-	_toggle_selection()
-
-
-func _toggle_selection():
-	if _hovered_items.is_empty():
-		return
-
-	var item = _hovered_items.back()
-	if item.selected:
-		_editor_global.data.remove_selected.call(item)
-	else:
-		_editor_global.data.add_selected.call(item)
-
-
-func _cancel():
-	_editor_global.data.clear_selected.call()
