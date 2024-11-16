7028a437d52fb0b2a88a1d1c6a3deb4feb5220a7
HotariTobu <hotari24tools@gmail.com>
2024-11-16 15:37:30 +0900

    feat: stoplight pos

--- src/editor/editor_stoplight/editor_property_panel_content_stoplight.gd
+++ src/editor/editor_stoplight/editor_property_panel_content_stoplight.gd
@@ -33,7 +33,17 @@ var _split_cells: Array[Control] = []:
 
 		_split_cells = new_split_cells
 
+@onready var _pos_panel: Vector2Panel = $PosPanel
+@onready var _x_box: NumericBox = _pos_panel.get_x_box()
+@onready var _y_box: NumericBox = _pos_panel.get_y_box()
+
 @onready var _offset_box = $OffsetBox
+
+
+func _ready():
+	_x_box.value_changed.connect(_on_x_box_value_changed)
+	_y_box.value_changed.connect(_on_y_box_value_changed)
+
 
 func get_target_type():
 	return EditorStoplightCore
@@ -42,43 +52,86 @@ func get_target_type():
 func activate():
 	super()
 	var converter = ItemsToSourcesConverter.new(EditorStoplightCore)
-	_editor_global.data.bind(&'selected_items').using(converter).to(self, &'stoplight_sources')
+	_editor_global.data.bind(&"selected_items").using(converter).to(self, &"stoplight_sources")
+
 
 func deactivate():
 	super()
-	_editor_global.data.unbind(&'selected_items').from(self, &'stoplight_sources')
+	_editor_global.data.unbind(&"selected_items").from(self, &"stoplight_sources")
 	stoplight_sources = []
+
 
 func _bind_cells(sources: Array[EditorBindingSource]):
 	var first_source = sources.front() as EditorBindingSource
 
-	var unity_converter = UnifyConverter.from_property(first_source, &'offset')
+	var x_of = func(pos: Vector2): return pos.x
+	var y_of = func(pos: Vector2): return pos.y
+
+	var unity_converter_x = UnifyConverter.new(func(): return first_source.pos.x)
+	var unity_converter_y = UnifyConverter.new(func(): return first_source.pos.y)
+
+	var unity_converter = UnifyConverter.from_property(first_source, &"offset")
 
 	for source in sources:
-		source.bind(&'offset').using(unity_converter).to(_offset_box, &'value')
+		source.bind(&"pos").using(x_of).using(unity_converter_x).to(_x_box, &"value")
+		source.bind(&"pos").using(y_of).using(unity_converter_y).to(_y_box, &"value")
+
+		source.bind(&"offset").using(unity_converter).to(_offset_box, &"value")
 
 	if len(sources) == 1:
 		var split_cell_creator = SplitCellCreator.new(first_source)
-		first_source.bind(&'split_ids').using(split_cell_creator).to(self, &'_split_cells')
+		first_source.bind(&"split_ids").using(split_cell_creator).to(self, &"_split_cells")
+
 
 func _unbind_cells(sources: Array[EditorBindingSource]):
 	var first_source = sources.front() as EditorBindingSource
 
 	for source in sources:
-		source.unbind(&'offset').from(_offset_box, &'value')
+		source.unbind(&"pos").from(_x_box, &"value")
+		source.unbind(&"pos").from(_y_box, &"value")
+
+		source.unbind(&"offset").from(_offset_box, &"value")
 
 	if len(sources) == 1:
-		first_source.unbind(&'split_ids').from(self, &'_split_cells')
+		first_source.unbind(&"split_ids").from(self, &"_split_cells")
 		_split_cells = []
+
+
+func _on_x_box_value_changed(new_value):
+	_editor_global.undo_redo.create_action("Change stoplight pos x")
+
+	for source in stoplight_sources:
+		var prev = source.pos as Vector2
+		var next = Vector2(new_value, prev.y)
+
+		_editor_global.undo_redo.add_do_property(source, &"pos", next)
+		_editor_global.undo_redo.add_undo_property(source, &"pos", prev)
+
+	_editor_global.undo_redo.commit_action()
+
+
+func _on_y_box_value_changed(new_value):
+	_editor_global.undo_redo.create_action("Change stoplight pos y")
+
+	for source in stoplight_sources:
+		var prev = source.pos as Vector2
+		var next = Vector2(prev.x, new_value)
+
+		_editor_global.undo_redo.add_do_property(source, &"pos", next)
+		_editor_global.undo_redo.add_undo_property(source, &"pos", prev)
+
+	_editor_global.undo_redo.commit_action()
+
 
 func _on_offset_box_value_changed(new_value: float):
 	_editor_global.undo_redo.create_action("Change stoplight offset")
 
 	for source in stoplight_sources:
-		_editor_global.undo_redo.add_do_property(source, &'offset', new_value)
-		_editor_global.undo_redo.add_undo_property(source, &'offset', source.offset)
+		_editor_global.undo_redo.add_do_property(source, &"offset", new_value)
+		_editor_global.undo_redo.add_undo_property(source, &"offset", source.offset)
 
 	_editor_global.undo_redo.commit_action()
+
 
 class SplitCellCreator:
 	extends BindingConverter
@@ -111,7 +164,6 @@ class SplitCellCreator:
 
 		return split_cells
 
-
 	func _create_split_cell(split: SplitData) -> Array[Control]:
 		var split_source = _editor_global.source_db.get_or_add(split)
 		var stoplight_sector_node = _editor_global.get_content_node(split.id) as EditorStoplightSector
@@ -124,7 +176,7 @@ class SplitCellCreator:
 		_connect_hover_signals_for_selecting(duration_label, stoplight_sector_node)
 
 		var duration_box = DurationBox.new()
-		split_source.bind(&'duration').to(duration_box, &'value')
+		split_source.bind(&"duration").to(duration_box, &"value")
 		_connect_hover_signals_for_selecting(duration_box, stoplight_sector_node)
 		duration_box.value_changed.connect(_on_duration_box_value_changed.bind(split_source))
 
@@ -185,6 +237,7 @@ class SplitCellCreator:
 	func _connect_hover_signals_for_selecting(control: Control, selectable: EditorSelectable):
 		control.mouse_entered.connect(func(): selectable.selecting = true)
 		control.mouse_exited.connect(func(): selectable.selecting = false)
+
 
 class DurationBox:
 	extends NumericBox
