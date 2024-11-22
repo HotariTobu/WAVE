55ffb892130584d9ea94e4ddedd64cf01cec1075
HotariTobu <hotari24tools@gmail.com>
2024-11-22 11:53:53 +0900

    upd: parent -> HBox

--- src/simulator/parameter_panel.gd
+++ src/simulator/parameter_panel.gd
@@ -17,12 +17,12 @@ func _ready():
 	_parameter.vehicle_spawn_after_start = setting.default_vehicle_spawn_after_start
 	_parameter.vehicle_spawn_limit = setting.default_vehicle_spawn_limit
 
-	_source.bind(&"step_delta").to($StepDeltaBox, &"value")
-	_source.bind(&"max_step").to($MaxStepBox, &"value")
+	_source.bind(&"step_delta").to(%StepDeltaBox, &"value")
+	_source.bind(&"max_step").to(%MaxStepBox, &"value")
 
-	_source.bind(&"vehicle_spawn_before_start").to_check_button($VehicleSpawnBeforeStartSwitch)
-	_source.bind(&"vehicle_spawn_after_start").to_check_button($VehicleSpawnAfterStartSwitch)
-	_source.bind(&"vehicle_spawn_limit").to($VehicleSpawnLimitBox, &"value")
+	_source.bind(&"vehicle_spawn_before_start").to_check_button(%VehicleSpawnBeforeStartSwitch)
+	_source.bind(&"vehicle_spawn_after_start").to_check_button(%VehicleSpawnAfterStartSwitch)
+	_source.bind(&"vehicle_spawn_limit").to(%VehicleSpawnLimitBox, &"value")
 
 	for child in get_children():
 		if child is LineEdit:
