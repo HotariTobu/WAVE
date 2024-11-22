15e166d8cdaaf11187d12785bc8da20b217c998f
HotariTobu <hotari24tools@gmail.com>
2024-11-22 11:53:29 +0900

    add: vehicle attrs

--- src/common/parameter_data.gd
+++ src/common/parameter_data.gd
@@ -7,6 +7,18 @@ var vehicle_spawn_before_start: bool
 var vehicle_spawn_after_start: bool
 var vehicle_spawn_limit: int
 
+var vehicle_length_options: Array[RandomOption]
+
+var vehicle_relative_speed_range: IntRange
+var vehicle_relative_speed_average: int
+var vehicle_max_speed_range: IntRange
+var vehicle_max_speed_average: int
+var vehicle_min_following_distance_range: IntRange
+var vehicle_min_following_distance_average: int
+var vehicle_max_following_distance_range: IntRange
+var vehicle_max_following_distance_average: int
+
+
 static func to_dict(data: ParameterData) -> Dictionary:
 	return {
 		&"step_delta": data.step_delta,
@@ -14,6 +26,15 @@ static func to_dict(data: ParameterData) -> Dictionary:
 		&"vehicle_spawn_before_start": data.vehicle_spawn_before_start,
 		&"vehicle_spawn_after_start": data.vehicle_spawn_after_start,
 		&"vehicle_spawn_limit": data.vehicle_spawn_limit,
+		&"vehicle_length_options": data.vehicle_length_options.map(RandomOption.to_dict),
+		&"vehicle_relative_speed_range": IntRange.to_dict(data.vehicle_relative_speed_range),
+		&"vehicle_relative_speed_average": data.vehicle_relative_speed_average,
+		&"vehicle_max_speed_range": IntRange.to_dict(data.vehicle_max_speed_range),
+		&"vehicle_max_speed_average": data.vehicle_max_speed_average,
+		&"vehicle_min_following_distance_range": IntRange.to_dict(data.vehicle_min_following_distance_range),
+		&"vehicle_min_following_distance_average": data.vehicle_min_following_distance_average,
+		&"vehicle_max_following_distance_range": IntRange.to_dict(data.vehicle_max_following_distance_range),
+		&"vehicle_max_following_distance_average": data.vehicle_max_following_distance_average,
 	}
 
 
@@ -24,4 +45,53 @@ static func from_dict(dict: Dictionary) -> ParameterData:
 	data.vehicle_spawn_before_start = dict.get(&"vehicle_spawn_before_start", NAN)
 	data.vehicle_spawn_after_start = dict.get(&"vehicle_spawn_after_start", NAN)
 	data.vehicle_spawn_limit = dict.get(&"vehicle_spawn_limit", NAN)
+	data.vehicle_length_options.assign(dict.get(&"vehicle_length_options", NAN).map(RandomOption.from_dict))
+	data.vehicle_relative_speed_range = IntRange.from_dict(dict.get(&"vehicle_relative_speed_range", NAN))
+	data.vehicle_relative_speed_average = dict.get(&"vehicle_relative_speed_average", NAN)
+	data.vehicle_max_speed_range = IntRange.from_dict(dict.get(&"vehicle_max_speed_range", NAN))
+	data.vehicle_max_speed_average = dict.get(&"vehicle_max_speed_average", NAN)
+	data.vehicle_min_following_distance_range = IntRange.from_dict(dict.get(&"vehicle_min_following_distance_range", NAN))
+	data.vehicle_min_following_distance_average = dict.get(&"vehicle_min_following_distance_average", NAN)
+	data.vehicle_max_following_distance_range = IntRange.from_dict(dict.get(&"vehicle_max_following_distance_range", NAN))
+	data.vehicle_max_following_distance_average = dict.get(&"vehicle_max_following_distance_average", NAN)
 	return data
+
+
+class RandomOption:
+	var value: Variant
+	var weight: float
+
+	func _init(_value: Variant, _weight: float):
+		value = _value
+		weight = _weight
+
+	static func to_dict(data: RandomOption) -> Dictionary:
+		return {
+			&"value": data.value,
+			&"weight": data.weight,
+		}
+
+	static func from_dict(dict: Dictionary) -> RandomOption:
+		var value = dict.get(&"value", NAN)
+		var weight = dict.get(&"weight", NAN)
+		return RandomOption.new(value, weight)
+
+
+class IntRange:
+	var begin: int
+	var end: int
+
+	func _init(_begin: int, _end: int):
+		begin = _begin
+		end = _end
+
+	static func to_dict(data: IntRange) -> Dictionary:
+		return {
+			&"begin": data.begin,
+			&"end": data.end,
+		}
+
+	static func from_dict(dict: Dictionary) -> IntRange:
+		var begin = dict.get(&"begin", NAN)
+		var end = dict.get(&"end", NAN)
+		return IntRange.new(begin, end)
--- src/common/setting.gd
+++ src/common/setting.gd
@@ -38,6 +38,32 @@ var lane_block_targeting_color = Color(lane_block_targeted_color, 0.5)
 var default_step_delta = 10
 var default_max_step = 10
 
-var default_vehicle_spawn_before_start: = true
+var default_vehicle_spawn_before_start := true
 var default_vehicle_spawn_after_start = true
 var default_vehicle_spawn_limit = 1
+
+var default_vehicle_length_options = [
+	ParameterData.RandomOption.new(2.5, 6.7),
+	ParameterData.RandomOption.new(3.0, 6.7),
+	ParameterData.RandomOption.new(3.4, 6.7),
+	ParameterData.RandomOption.new(4.1, 138.3),
+	ParameterData.RandomOption.new(4.7, 138.3),
+	ParameterData.RandomOption.new(4.8, 138.3),
+	ParameterData.RandomOption.new(5.0, 44),
+	ParameterData.RandomOption.new(6.0, 77),
+	ParameterData.RandomOption.new(7.0, 77),
+	ParameterData.RandomOption.new(8.0, 28.5),
+	ParameterData.RandomOption.new(9.0, 28.5),
+	ParameterData.RandomOption.new(10.0, 24.5),
+	ParameterData.RandomOption.new(11.0, 24.5),
+	ParameterData.RandomOption.new(12.0, 20),
+]
+
+var default_vehicle_relative_speed_range = ParameterData.IntRange.new(0, 30)
+var default_vehicle_relative_speed_average = 10
+var default_vehicle_max_speed_range = ParameterData.IntRange.new(80, 150)
+var default_vehicle_max_speed_average = 110
+var default_vehicle_min_following_distance_range = ParameterData.IntRange.new(1, 5)
+var default_vehicle_min_following_distance_average = 2
+var default_vehicle_max_following_distance_range = ParameterData.IntRange.new(10, 100)
+var default_vehicle_max_following_distance_average = 50
