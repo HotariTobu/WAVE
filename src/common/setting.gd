5f6ccdde53f9ee28004ae16706664a857a8525df
HotariTobu <hotari24tools@gmail.com>
2024-11-25 17:05:48 +0900

    upd: max_step 10 -> 100

--- src/common/setting.gd
+++ src/common/setting.gd
@@ -43,7 +43,7 @@ var lane_block_targeted_color = Color("#f01616")
 var lane_block_targeting_color = Color(lane_block_targeted_color, 0.5)
 
 var default_step_delta = 1
-var default_max_step = 10
+var default_max_step = 100
 var default_random_seed = 5
 
 var default_vehicle_spawn_before_start := true
