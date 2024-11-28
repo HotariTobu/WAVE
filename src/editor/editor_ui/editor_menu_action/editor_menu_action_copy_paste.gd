bc743ed19fe93ababdb0183ffe64a9b4b9d3f783
HotariTobu <hotari24tools@gmail.com>
2024-11-28 09:46:33 +0900

    feat: paste offset

--- src/editor/editor_ui/editor_menu_action/editor_menu_action_copy_paste.gd
+++ src/editor/editor_ui/editor_menu_action/editor_menu_action_copy_paste.gd
@@ -4,7 +4,8 @@ const PASTE_OFFSET = 10
 
 var _editor_global = editor_global
 
-var _last_paste_offset: Vector2
+var _last_paste_anchor_pos: Vector2
+var _last_paste_offset: float
 
 
 func copy_selection():
@@ -52,10 +53,30 @@ func paste(selected_only: bool):
 	if anchor_pos_dict == null or anchor_pos_dict is not Dictionary:
 		return
 
-	var anchor_pos = VertexData.Pos.form_dict(anchor_pos_dict)
-	if not anchor_pos.is_finite():
+	var copy_anchor_pos = VertexData.Pos.form_dict(anchor_pos_dict)
+	if not copy_anchor_pos.is_finite():
 		return
 
+	var clone_result = _clone(sub_network_dict, selected_only)
+
+	_editor_global.undo_redo.create_action("Paste")
+
+	for group_name in clone_result.group_name_contents_dict:
+		var group = _editor_global.content_db.get_group(group_name)
+		var contents = clone_result.group_name_contents_dict[group_name]
+
+		for content in contents:
+			_editor_global.undo_redo.add_do_method(group.add.bind(content))
+			_editor_global.undo_redo.add_undo_method(group.remove.bind(content))
+
+	_editor_global.undo_redo.add_do_method(_select_pasted_content_nodes.bind(clone_result.new_ids))
+
+	_editor_global.undo_redo.commit_action()
+
+	_move.call_deferred(clone_result.group_name_contents_dict, copy_anchor_pos)
+
+
+func _clone(sub_network_dict: Dictionary, _selected_only: bool):
 	var group_name_contents_dict: Dictionary
 	var new_id_dict: Dictionary
 
@@ -79,23 +100,22 @@ func paste(selected_only: bool):
 
 		group_name_contents_dict[group_name] = contents
 
-	#if selected_only:
-	#var selected_id_set = Set.from_array(selected_ids)
-	#var selected_only_new_id_dict: Dictionary
-#
-	#for old_id in new_id_dict:
-	#if not selected_id_set.has(old_id):
-	#continue
-#
-	#var new_id = new_id_dict[old_id]
-	#selected_only_new_id_dict[old_id] = new_id
-#
-	#new_id_dict = selected_only_new_id_dict
+	# if selected_only:
+	# 	var selected_id_set = Set.from_array(selected_ids)
+	# 	var selected_only_new_id_dict: Dictionary
+
+	# for old_id in new_id_dict:
+	# 	if not selected_id_set.has(old_id):
+	# 		continue
+
+	# var new_id = new_id_dict[old_id]
+	# selected_only_new_id_dict[old_id] = new_id
+
+	# new_id_dict = selected_only_new_id_dict
 
 	new_id_dict.make_read_only()
 
 	var new_id_of = func(old_id: StringName): return new_id_dict.get(old_id)
-	var paste_offset = _get_paste_offset()
 
 	for group_name in group_name_contents_dict:
 		var contents = group_name_contents_dict[group_name]
@@ -104,19 +124,42 @@ func paste(selected_only: bool):
 		for content in contents:
 			helper_script.replace_id(content, new_id_of)
 
-	_editor_global.undo_redo.create_action("Paste")
+	var result = CloneResult.new()
+	result.group_name_contents_dict = group_name_contents_dict
+	result.new_ids.assign(new_id_dict.values())
 
-	for group_name in group_name_contents_dict:
-		var contents = group_name_contents_dict[group_name]
-		var group = _editor_global.content_db.get_group(group_name)
+	return result
 
-		for content in contents:
-			_editor_global.undo_redo.add_do_method(group.add.bind(content))
-			_editor_global.undo_redo.add_undo_method(group.remove.bind(content))
 
-	_editor_global.undo_redo.add_do_method(_select_pasted_content_nodes.bind(new_id_dict.values()))
+func _move(group_name_contents_dict: Dictionary, copy_anchor_pos: Vector2) -> void:
+	var all_contents: Array[ContentData]
 
-	_editor_global.undo_redo.commit_action()
+	for contents in group_name_contents_dict.values():
+		all_contents.append_array(contents)
+
+	var movable_id_set = Set.new()
+
+	for content in all_contents:
+		var constraint = _editor_global.constraint_db.of(content.id)
+		constraint.merge_movable_id_set_to(movable_id_set)
+
+	if movable_id_set.is_empty():
+		return
+
+	var paste_anchor_pos = _get_anchor_pos()
+	if _last_paste_anchor_pos != paste_anchor_pos:
+		_last_paste_anchor_pos = paste_anchor_pos
+		_last_paste_offset = 0
+
+	_last_paste_offset += _get_paste_offset()
+	var paste_offset = paste_anchor_pos - copy_anchor_pos + Vector2.ONE * _last_paste_offset
+
+	for content_id in movable_id_set.to_array():
+		var group_name = _editor_global.content_db.group_name_of(content_id)
+		var helper_script = EditorScriptDict.helper[group_name]
+		var content = _editor_global.content_db.data_of(content_id)
+		var source = _editor_global.source_db.get_or_add(content)
+		helper_script.move_by(source, paste_offset)
 
 
 func _get_anchor_pos():
@@ -127,8 +170,13 @@ func _get_paste_offset():
 	return PASTE_OFFSET / _editor_global.camera.zoom_value
 
 
-func _select_pasted_content_nodes(content_ids: Array):
+func _select_pasted_content_nodes(content_ids: Array[StringName]):
 	var content_nodes: Array[EditorContent]
 	content_nodes.assign(content_ids.map(_editor_global.content_node_of))
 	_editor_global.data.clear_selected.call()
 	_editor_global.data.add_all_selected.call(content_nodes)
+
+
+class CloneResult:
+	var group_name_contents_dict: Dictionary
+	var new_ids: Array[StringName]
