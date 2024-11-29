3794ce72c90e327b6d895986650abbf99b0f8d4a
HotariTobu <hotari24tools@gmail.com>
2024-11-29 13:33:35 +0900

    upd: signal arg order

--- src/observable_array.gd
+++ src/observable_array.gd
@@ -2,8 +2,8 @@ class_name ObservableArray
 
 enum ReorderType { REVERSE, SHUFFLE, SORT }
 
-signal value_inserted(position: int, added_value: Variant)
-signal value_removed(position: int, removed_value: Variant)
+signal value_inserted(added_value: Variant, position: int)
+signal value_removed(removed_value: Variant, position: int)
 signal duplicated
 signal made_read_only
 signal reordered(type: ReorderType)
@@ -26,7 +26,7 @@ func any(method: Callable) -> bool:
 func append(value: Variant) -> void:
 	var position = len(_data)
 	_data.append(value)
-	value_inserted.emit(position, value)
+	value_inserted.emit(value, position)
 
 
 func append_array(array: Array) -> void:
@@ -37,7 +37,7 @@ func append_array(array: Array) -> void:
 	for offset in range(len(array)):
 		var position = old_size + offset
 		var added_value = array[offset]
-		value_inserted.emit(position, added_value)
+		value_inserted.emit(added_value, position)
 
 
 func assign(array: Array) -> void:
@@ -47,11 +47,11 @@ func assign(array: Array) -> void:
 
 	for position in range(len(clone)):
 		var removed_value = clone[position]
-		value_removed.emit(position, removed_value)
+		value_removed.emit(removed_value, position)
 
 	for position in range(len(array)):
 		var added_value = array[position]
-		value_inserted.emit(position, added_value)
+		value_inserted.emit(added_value, position)
 
 
 func back() -> Variant:
@@ -73,7 +73,7 @@ func clear() -> void:
 
 	for position in range(len(clone)):
 		var removed_value = clone[position]
-		value_removed.emit(position, removed_value)
+		value_removed.emit(removed_value, position)
 
 
 func count(value: Variant) -> int:
@@ -92,7 +92,7 @@ func erase(value: Variant) -> void:
 		return
 
 	_data.remove_at(position)
-	value_removed.emit(position, value)
+	value_removed.emit(value, position)
 
 
 func fill(value: Variant) -> void:
@@ -103,10 +103,10 @@ func fill(value: Variant) -> void:
 
 	for position in positions:
 		var removed_value = clone[position]
-		value_removed.emit(position, removed_value)
+		value_removed.emit(removed_value, position)
 
 	for position in positions:
-		value_inserted.emit(position, value)
+		value_inserted.emit(value, position)
 
 
 func filter(method: Callable) -> Array:
@@ -144,7 +144,7 @@ func hash() -> int:
 func insert(position: int, value: Variant) -> int:
 	var error = _data.insert(position, value)
 	if error == OK:
-		value_inserted.emit(position, value)
+		value_inserted.emit(value, position)
 	return error
 
 
@@ -187,31 +187,31 @@ func pick_random() -> Variant:
 
 func pop_at(position: int) -> Variant:
 	var value = _data.pop_at(position)
-	value_removed.emit(position, value)
+	value_removed.emit(value, position)
 	return value
 
 
 func pop_back() -> Variant:
 	var value = _data.pop_back()
-	value_removed.emit(len(_data), value)
+	value_removed.emit(value, len(_data))
 	return value
 
 
 func pop_front() -> Variant:
 	var value = _data.pop_front()
-	value_removed.emit(0, value)
+	value_removed.emit(value, 0)
 	return value
 
 
 func push_back(value: Variant) -> void:
 	var position = len(_data)
 	_data.push_back(value)
-	value_inserted.emit(position, value)
+	value_inserted.emit(value, position)
 
 
 func push_front(value: Variant) -> void:
 	_data.push_front(value)
-	value_inserted.emit(0, value)
+	value_inserted.emit(value, 0)
 
 
 func reduce(method: Callable, accum: Variant = null) -> Variant:
@@ -221,7 +221,7 @@ func reduce(method: Callable, accum: Variant = null) -> Variant:
 func remove_at(position: int) -> void:
 	var value = _data[position]
 	_data.remove_at(position)
-	value_removed.emit(position, value)
+	value_removed.emit(value, position)
 
 
 func resize(new_size: int) -> int:
@@ -230,13 +230,13 @@ func resize(new_size: int) -> int:
 
 	if old_size < new_size:
 		for position in range(old_size, new_size):
-			var emitter = value_inserted.emit.bind(position, null)
+			var emitter = value_inserted.emit.bind(null, position)
 			emitters.append(emitter)
 
 	else:
 		for position in range(new_size, old_size):
 			var value = _data[position]
-			var emitter = value_removed.emit.bind(position, value)
+			var emitter = value_removed.emit.bind(value, position)
 			emitters.append(emitter)
 
 	var error = _data.resize(new_size)
