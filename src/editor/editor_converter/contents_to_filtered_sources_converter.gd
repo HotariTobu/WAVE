79449d68148560b83a1acdcb3020473fd0af080c
HotariTobu <hotari24tools@gmail.com>
2024-12-06 09:30:40 +0900

    rm: isinstance

--- src/editor/editor_converter/contents_to_filtered_sources_converter.gd
+++ src/editor/editor_converter/contents_to_filtered_sources_converter.gd
@@ -18,16 +18,5 @@ func source_to_target(source_value: Variant) -> Variant:
 
 
 static func from_type(target_content_type: GDScript) -> ContentsToFilteredSourcesConverter:
-	var filter = isinstance.bind(target_content_type)
+	var filter = is_instance_of.bind(target_content_type)
 	return ContentsToFilteredSourcesConverter.new(filter)
-
-
-static func isinstance(instance: Object, script: Script) -> bool:
-	var instance_script = instance.get_script()
-	while instance_script != null:
-		if instance_script == script:
-			return true
-
-		instance_script = instance_script.get_base_script()
-
-	return false
