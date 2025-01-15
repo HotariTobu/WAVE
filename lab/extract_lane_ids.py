bd30e70821018e420f4268d92dbe441828e68243
HotariTobu <hotari24tools@gmail.com>
2025-01-15 22:53:38 +0900

    upd: to accept json file

--- lab/extract_lane_ids.py
+++ lab/extract_lane_ids.py
@@ -3,15 +3,24 @@ import json
 from pathlib import Path
 
 parser = argparse.ArgumentParser()
-parser.add_argument('output', type=Path, help='Path to output text file')
+parser.add_argument("output", type=Path, help="Path to output text file")
+parser.add_argument(
+    "-d", "--data", dest="data", type=Path, help="Path to input data json file"
+)
 
 args = parser.parse_args()
-output_path = args.output
+output_path: Path = args.output
+data_path: Path = args.data
 
-raw_data = input('Paste data...')
+if data_path:
+    with open(data_path, "r") as f:
+        raw_data = f.read()
+else:
+    raw_data = input("Paste data...")
+
 data = json.loads(raw_data)
 
-lane_ids = [lane['id'] for lane in data['lanes']]
+lane_ids = [lane["id"] for lane in data["lanes"]]
 
-with open(output_path, 'w') as f:
-    f.write('\n'.join(lane_ids))
+with open(output_path, "w") as f:
+    f.write("\n".join(lane_ids))
